e5060ba1d350f8b8244d59995749ab9fdc8a8a58
Fred Frey <fred@fpf3.net>
2024-08-16 19:13:03 -0400

    add xsandbox also clean up packages

--- users/fred/headless_pkgs.nix
+++ users/fred/headless_pkgs.nix
@@ -1,24 +1,24 @@
 {pkgs, ...}:
-[
-  pkgs.bc
-  pkgs.fd
-  pkgs.file
-  pkgs.gnumake
-  pkgs.htop
-  pkgs.lm_sensors
-  pkgs.ncdu
-  pkgs.openfortivpn
-  pkgs.ranger
-  pkgs.ripgrep
-  pkgs.screen
-  pkgs.sshfs
-  pkgs.sysstat
-  pkgs.tmux
-  pkgs.unzip
-  pkgs.tree
-  pkgs.wget
-  pkgs.yubikey-manager
-  pkgs.zsh
-  pkgs.man-pages
-  pkgs.man-pages-posix
+with pkgs; [
+  bc
+  fd
+  file
+  gnumake
+  htop
+  lm_sensors
+  ncdu
+  openfortivpn
+  ranger
+  ripgrep
+  screen
+  sshfs
+  sysstat
+  tmux
+  unzip
+  tree
+  wget
+  yubikey-manager
+  zsh
+  man-pages
+  man-pages-posix
 ]
