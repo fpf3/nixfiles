b844aaa4655d6d57cee88c95b6e6623b1ee5e737
Fred Frey <fred@fpf3.net>
2024-08-17 04:05:26 -0400

    add common.nix

---
+++ common.nix
@@ -0,0 +1,53 @@
+# Common settings between all machines
+{config, lib, pkgs, ...}:
+{
+  # ZFS wants this set. Why? XXX
+  networking.hostId = "b2c73136"; # just a random number...
+
+  # Set your time zone.
+  time.timeZone = "America/New_York";
+
+  # list of unfree $oftware to allow
+  nixpkgs.config.allowUnfreePredicate = pkg:
+  builtins.elem (lib.getName pkg) [
+      "joypixels"
+      "nvidia-persistenced"
+      "nvidia-settings"
+      "nvidia-x11"
+      "steam"
+      "steam-original"
+      "steam-run"
+    ];
+
+  # List packages installed in system profile.
+   environment.systemPackages = with pkgs; [
+     wget
+   ];
+
+  # fonts
+  fonts.packages = with pkgs; [
+    noto-fonts
+    noto-fonts-color-emoji
+    #joypixels # figure out how to accept license declaratively
+  ];
+
+  # enable avahi
+  services.avahi = {
+    enable = true;
+    
+    ipv4 = true;
+    nssmdns4 = true;
+
+    ipv6 = false;
+    nssmdns6 = false;
+
+    publish = {
+      enable = true;
+      domain = true;
+      addresses = true;
+    };
+
+    openFirewall = true;
+    domainName = "local";
+  };
+}
