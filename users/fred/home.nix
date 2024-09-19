80c60418d07c0c247a64a195d62124189d905eb5
Fred Frey <fred@fpf3.net>
2024-09-19 00:48:18 -0400

    xresources

--- users/fred/home.nix
+++ users/fred/home.nix
@@ -58,6 +58,10 @@
     vim = (import ./vim.nix) { pkgs=pkgs; lib=lib; };
   };
 
+  xresources.properties = {
+    "st.alpha" = "1.0";
+  };
+
   dconf = lib.mkIf(dconf != {}) dconf;
   gtk = lib.mkIf(gtk != {}) gtk;
   home.pointerCursor = lib.mkIf (pointerCursor != {}) pointerCursor;
