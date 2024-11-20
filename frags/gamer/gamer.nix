f02b654cf666903f626e42bfcebccc4ca91f5458
Fred Frey <fred@fpf3.net>
2024-11-20 00:25:28 -0500

    forgot to add gamer frag

---
+++ frags/gamer/gamer.nix
@@ -0,0 +1,10 @@
+{pkgs, lib, config, withGui ? true}:
+{
+  # steam system config
+  programs.steam = lib.mkIf(withGui) {
+    enable = true;
+    remotePlay.openFirewall = true; # Open ports in the firewall for Steam Remote Play
+    dedicatedServer.openFirewall = true; # Open ports in the firewall for Source Dedicated Server
+    localNetworkGameTransfers.openFirewall = true; # Open ports in the firewall for Steam Local Network Game Transfers
+  };
+}
