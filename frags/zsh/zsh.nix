176fc938669773752f5e3ec788bac8c8d6af7c9e
Fred Frey <f.frey@qvii.com>
2025-05-30 10:29:28 -0400

    support 24.11

--- frags/zsh/zsh.nix
+++ frags/zsh/zsh.nix
@@ -1,21 +1,27 @@
 {lib, envVars ? {}}:
 let
   customdir = builtins.path { path=./custom; };
+  old_init = builtins.substring 0 5 (lib.version) <= "24.11"; # XXX remove once everything is off 24.11
 in
 {
   enable = true;
-  #antidote.enable = true;
-  #antidote.plugins = [ ohmyzsh/ohmyzsh ];
   oh-my-zsh = {
     enable = true;
     custom = "${customdir}";
     theme = "fpf3-half-life";
   };
   autocd = true;
-  initContent = ''
+  sessionVariables = lib.mkIf(envVars != {}) envVars;
+}
+// lib.optionalAttrs (old_init) {
+initExtra = lib.mkIf(old_init) ''
+  #NIX_AUTO_RUN = "1"
+  PATH=$PATH:/home/fred/bin
+'';
+}
+// lib.optionalAttrs(!old_init) {
+  initContent = lib.mkIf(!old_init) ''
     #NIX_AUTO_RUN = "1"
     PATH=$PATH:/home/fred/bin
   '';
-
-  sessionVariables = lib.mkIf(envVars != {}) envVars;
 }
