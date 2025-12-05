aa160bd114dc91baa2b0a45302b6c57ed1ccdb1c
Fred Frey <fred@fpf3.net>
2025-12-05 02:27:55 -0500

    add overlays

---
+++ frags/pkgs/overlays.nix
@@ -0,0 +1,12 @@
+{ pkgs, lib }:
+[
+  (final: prev: {
+    brlaser = prev.brlaser.overrideAttrs (oldAttrs: {
+      postPatch = ''
+      substituteInPlace CMakeLists.txt --replace-fail \
+      'cmake_minimum_required(VERSION 3.1)' \
+      'cmake_minimum_required(VERSION 4.0)'
+      '';
+    });
+  })
+]
