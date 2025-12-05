892d335cf50b9fb8bb77a65ea6338ea02d5f5eab
Fred Frey <fred@fpf3.net>
2025-12-05 03:23:21 -0500

    disable wayland

--- frags/home/home.nix
+++ frags/home/home.nix
@@ -77,11 +77,11 @@ in
 
   programs.bash.sessionVariables = lib.mkIf(envVars != {}) envVars;
 
-  wayland.windowManager.hyprland = {
-    enable = true;
-    xwayland.enable = true;
-    extraConfig = lib.readFile ../hyprland/hyprland.conf;
-  };
+  #wayland.windowManager.hyprland = {
+  #  enable = true;
+  #  xwayland.enable = true;
+  #  extraConfig = lib.readFile ../hyprland/hyprland.conf;
+  #};
 
   dconf = lib.mkIf(dconf != {}) dconf;
   gtk = lib.mkIf(gtk != {}) gtk;
